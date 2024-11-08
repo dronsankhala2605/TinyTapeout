110875492e2c84377e956e7961ca82c8b8e5b705
dronsankhala2605 <2024pvl0098@iitjammu.ac.in>
2024-11-08 22:45:45 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-// Replace tt_um_example with your module name:
-  tt_um_devider user_project (
+  // Replace tt_um_example with your module name:
+  tt_um_c_2_array_mult user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -36,14 +36,15 @@ module tb ();
       .VGND(VGND),
 `endif
 
-      .ui_in  (a),    // Dedicated inputs
-      .uo_out (sum),   // Dedicated outputs
-      .uio_in (b),   // IOs: Input path
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
+
 
 endmodule
