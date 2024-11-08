81a820166309a089682d8a9e0bf0649bc24d7372
dronsankhala2605 <2024pvl0098@iitjammu.ac.in>
2024-11-08 22:17:02 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,10 +1,11 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
-module test_tb();
+module tb ();
+
+  // Declare continuous signals for power and ground
+  wire VPWR = 1'b1;  // Power supply
+  wire VGND = 1'b0;  // Ground
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
@@ -14,26 +15,43 @@ module test_tb();
   end
 
   // Wire up the inputs and outputs:
- reg clock,reset;
-wire [3:0]count;
+  reg clk;
+  reg rst_n;
+  reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
+  wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
-tt_um_test dut(.clock(clock),.reset(reset),.count(count));
+  // Clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk; // 10 ns clock period
+  end
 
-initial
- begin
-      reset = 1'b0;
-  #10 reset = 1'b1;
-  #10 reset = 1'b0;
- end
- 
-initial
- begin
-      clock = 1'b0;
-  #40 clock = 1'b1;
-  forever #40 clock = ~clock;
- end
- 
-initial
- #500 $finish;
- 
+  // Reset and enable signals
+  initial begin
+    rst_n = 0;
+    ena = 0;
+    #10 rst_n = 1; // Release reset after 10ns
+    #10 ena = 1;   // Enable after 20ns
+  end
+
+  // Instantiate your ALU module
+  tt_um_alf19185_ALU U1 (
+`ifdef GL_TEST
+      .VPWR(VPWR),  // Connect to power using wire
+      .VGND(VGND),  // Connect to ground using wire
+`endif
+      .ui_in  (ui_in),
+      .uo_out (uo_out),
+      .uio_in (uio_in),
+      .uio_out(uio_out),
+      .uio_oe (uio_oe),
+      .ena    (ena),
+      .clk    (clk),
+      .rst_n  (rst_n)
+  );
+
 endmodule
